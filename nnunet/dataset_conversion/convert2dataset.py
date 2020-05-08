61b195a40faccbad2a7b4b600a63ade04253c6a2
安兴乐😊 <anxingle@cecbrain.com>
2020-05-08 15:38:40 +0800

    feat: 数据集转换

---
+++ nnunet/dataset_conversion/convert2dataset.py
@@ -0,0 +1,36 @@
+from copy import deepcopy
+from pathlib import Path
+from batchgenerators.utilities.file_and_folder_operations import *
+import shutil
+
+
+
+if __name__ == "__main__":
+	labels_base = "/media/fabian/My Book/datasets/KiTS2019_Challenge/kits19/data"
+	out = "/media/fabian/My Book/MedicalDecathlon/nnUNet_raw_splitted/Task040_KiTS"
+
+	json_dict = {}
+	json_dict['name'] = "MyData"
+	json_dict['description'] = "Use nnUNet to train"
+	json_dict['tensorImageSize'] = "4D"
+	json_dict['reference'] = "KiTS data for nnunet"
+	json_dict['licence'] = ""
+	json_dict['release'] = "0.0"
+	json_dict['modality'] = {
+		"0": "CT",
+	}
+	json_dict['labels'] = {
+		"0": "background",
+		"1": "Kidney",
+		"2": "Tumor"
+	}
+
+	cases = [f.name for f in Path(labels_base).iterdir() if f.is_file() and ".nii.gz" in f.name]
+	json_dict['numTraining'] = len(cases)
+	json_dict['numTest'] = 0
+	json_dict['training'] = [{'image': "./imagesTr/%s.nii.gz" % i, "label": "./labelsTr/%s.nii.gz" % i} for i in
+							 cases]
+	json_dict['test'] = []
+
+	save_json(json_dict, os.path.join(out, "dataset.json"))
+
